cf3d9aeb9fb382cacdac5ef38cd2f4d5f0d337c5
unknown <elielelano7@gmail.com>
2023-05-25 01:31:43 -0300

    end

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -6,16 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent {
-  curtidas: number = 800;
-  verif: boolean = true
-  
+  curtidas: number = 799;
+  verif: boolean = false;
+  teste: boolean = false;
+
   curtir(): void {
     this.verif = !this.verif
-
     if (this.verif) {
       this.curtidas++
     } else {
       this.curtidas--
     }
+  }
+
+  active(): void{
+    this.teste = !this.teste
   }
 }
